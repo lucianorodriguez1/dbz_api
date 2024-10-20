1a6e5dac0068b291702fa6cf4811b81d0749ff34
luciano <lucianorodriguez1@users.noreply.github.com>
2024-10-20 19:46:37 -0300

    cambiado de diseño en main

--- app/src/main/java/com/example/dbz_api/MainActivity.kt
+++ app/src/main/java/com/example/dbz_api/MainActivity.kt
@@ -27,6 +27,7 @@ class MainActivity : AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
